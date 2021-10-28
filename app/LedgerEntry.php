0ba089fa5ea7156f871f53e7045d0010ef8e2d7a
Adriano <adrianoapi@hotmail.com>
2021-10-28 10:43:03 -0300

    remove money format

--- app/LedgerEntry.php
+++ app/LedgerEntry.php
@@ -24,16 +24,4 @@ class LedgerEntry extends Model
     {
         return $this->hasOne(TransitionType::class, 'id', 'transition_type_id');
     }
-
-    public function setEntryDateAttribute($value)
-    {
-        $date = str_replace('/', '-', $value);
-        return $this->attributes['entry_date'] = date("Y-m-d", strtotime($date));
-    }
-
-    public function setAmountAttribute($value)
-    {
-        return $this->attributes['amount'] = str_replace(',', '.', str_replace('.', '', $value));
-    }
-
 }
