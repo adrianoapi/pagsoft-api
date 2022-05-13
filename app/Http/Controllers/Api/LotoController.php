661e61ad942c2168e677af753b8155ab266ad6f7
Adriano A Costa <adrianoapi@hotmail.com>
2022-05-13 17:54:05 -0300

    create request jogo

---
+++ app/Http/Controllers/Api/LotoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LotoController extends Controller
+{
+    public function index(Request $request)
+    {
+        $result = $this->getResult($request->jogo);
+
+        $matriz = [
+            'numero' => $result->numero,
+            'sorteio' => $this->dezenas($result->listaDezenas),
+            'dataAppuracao' => $result->dataApuracao,
+            'listaRateioPremio' => $this->getListaRateioPremio($result->listaRateioPremio)
+        ];
+
+        echo json_encode($matriz);        
+    }
+
+    private function getResult($jogo, $concurso = NULL)
+    {
+        if(!empty($concurso)){
+           $concurso = "/{$concurso}";
+        }
+   
+       return json_decode(file_get_contents('https://servicebus2.caixa.gov.br/portaldeloterias/api/'.$jogo.$concurso));
+    }
+
+        
+    /**
+     * Retorna as dezenas prontas para inserir na base
+     */
+    function dezenas(array $data)
+    {
+        $result = NULL;
+        $i      = 0;
+
+        foreach($data as $value):
+            $number = substr($value, 1, 2);
+            $result .= $i > 0 ? " $number" : $number;
+            $i++;
+        endforeach;
+
+        return $result;
+    }
+
+    function getListaRateioPremio(array $data)
+    {
+        $result = [];
+        $i      = 1;
+
+        foreach($data as $value):
+            $result[] = [
+                "ganhador_{$value->faixa}" => $value->numeroDeGanhadores,
+                "rateio_{$value->faixa}"   => $value->valorPremio
+            ];
+            $i++;
+        endforeach;
+
+        return $result;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', 'Api\\AuthController@login');
 
+Route::get ('loto/resultado/{jogo}',  'Api\\LotoController@index');
+
 Route::group(['middleware' => ['apiJwt']], function(){
 
     Route::get ('auth/collection', 'Api\\UserController@getCollection');
