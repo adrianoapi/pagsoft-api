324fdd6ac1d1d837dd5fbcb7c2c1e5e0d74f533e
Adriano A Costa <adrianoapi@hotmail.com>
2022-07-17 19:09:08 -0300

    try fixed issue cacert

--- app/Http/Controllers/Api/LotoController.php
+++ app/Http/Controllers/Api/LotoController.php
@@ -26,8 +26,22 @@ class LotoController extends Controller
         if(!empty($concurso)){
            $concurso = "/{$concurso}";
         }
-   
-       return json_decode(file_get_contents('https://servicebus2.caixa.gov.br/portaldeloterias/api/'.$jogo.$concurso));
+
+        $arrContextOptions= [
+            'ssl' => [
+                'cafile' => 'cacert.pem',
+                'verify_peer'=> false,
+                'verify_peer_name'=> false,
+            ],
+        ];
+        
+        return json_decode($response = file_get_contents(
+            'https://servicebus2.caixa.gov.br/portaldeloterias/api/'.$jogo.$concurso,
+            false,
+            stream_context_create($arrContextOptions)
+        ));
+        
+        //return json_decode(file_get_contents('https://servicebus2.caixa.gov.br/portaldeloterias/api/'.$jogo.$concurso));
     }
 
         
